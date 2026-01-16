21270f54f22b1f30355776834a6152e6d16bdd05
Andrew Siminiuс <44125451+SimAndrew@users.noreply.github.com>
2026-01-16 19:51:27 +0200

    feat: add reducers remove item functionality to CartItem component and cartSlice

--- src/components/CartItem.jsx
+++ src/components/CartItem.jsx
@@ -1,6 +1,10 @@
+import { useDispatch } from 'react-redux';
+import { removeItem } from '../features/cart/cartSlice.js';
 import { ChevronDown, ChevronUp } from '../icons.jsx';
 
 const CartItem = ({ id, img, title, price, amount }) => {
+	const dispatch = useDispatch();
+
 	return (
 		<article className="cart-item">
 			<img src={img} alt={title} />
@@ -8,7 +12,14 @@ const CartItem = ({ id, img, title, price, amount }) => {
 			<div>
 				<h4>{title}</h4>
 				<h4 className="item-price">${price}</h4>
-				<button className="remove-btn">remove</button>
+				<button
+					onClick={() => {
+						dispatch(removeItem(id));
+					}}
+					className="remove-btn"
+				>
+					remove
+				</button>
 			</div>
 
 			<div>
