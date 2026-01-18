36c9970fe7014c803d38d04c5e86c3b34c1856f5
Andrew Siminiuс <44125451+SimAndrew@users.noreply.github.com>
2026-01-18 02:54:28 +0200

    feat: add calculateTotals reducer and implement total calculation in CartContainer

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,17 @@
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import Navbar from './components/Navbar.jsx';
 import CartContainer from './components/CartContainer.jsx';
+import { calculateTotals } from './features/cart/cartSlice.js';
 
 function App() {
+	const { cartItems } = useSelector((store) => store.cart);
+	const dispatch = useDispatch();
+
+	useEffect(() => {
+		dispatch(calculateTotals());
+	}, [cartItems, dispatch]);
+
 	return (
 		<main>
 			<Navbar />;
