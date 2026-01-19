899ca5871b259bd8df9d8bb9d5740767dcd5c659
Andrew Siminiuс <44125451+SimAndrew@users.noreply.github.com>
2026-01-19 17:03:10 +0200

    feat: implemented asynchronous product loading and shopping cart management via redux-toolkit and external api

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,29 @@ import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import Navbar from './components/Navbar.jsx';
 import CartContainer from './components/CartContainer.jsx';
-import { calculateTotals } from './features/cart/cartSlice.js';
+import { calculateTotals, getCartItems } from './features/cart/cartSlice.js';
 import Modal from './components/Modal.jsx';
 
 function App() {
-	const { cartItems } = useSelector((store) => store.cart);
+	const { cartItems, isLoading } = useSelector((store) => store.cart);
 	const { isOpen } = useSelector((store) => store.modal);
 	const dispatch = useDispatch();
 
 	useEffect(() => {
 		dispatch(calculateTotals());
 	}, [cartItems, dispatch]);
+
+	useEffect(() => {
+		dispatch(getCartItems());
+	}, [dispatch]);
+
+	if (isLoading) {
+		return (
+			<div className="loading">
+				<h3>loading...</h3>
+			</div>
+		);
+	}
 
 	return (
 		<main>
